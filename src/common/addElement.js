e68d0abcfd746147aa4e9de12d311628dcc87a4a
rbarannikirum <rbarannikitrum@gmail.com>
2023-12-01 11:55:05 +0300

    fix

--- src/common/addElement.js
+++ src/common/addElement.js
@@ -1,89 +1,93 @@
 function logInput() {
-    const mainInput = document.getElementById("input");
-    if (mainInput.value.trim()) {
-        console.log(mainInput.value);
-        return;
-    }
-    alert("enter some text");
+  const mainInput = document.getElementById("input");
+  if (mainInput.value.trim()) {
+    console.log(mainInput.value);
+    return;
+  }
+  alert("enter some text");
 }
 
 function resetInput() {
-    const mainInput = document.getElementById("input");
-    // addElement(mainInput.value);
-    mainInput.value = "";
+  const mainInput = document.getElementById("input");
+  // addElement(mainInput.value);
+  mainInput.value = "";
 }
 
 function blockInput() {
-    const blockButton = document.getElementById("block");
-    const mainInput = document.getElementById("input");
-    if (blockButton.innerHTML === "block input") {
-        mainInput.setAttribute("disabled", true);
-        blockButton.innerHTML = "unlock input";
-        return;
-    }
-    mainInput.removeAttribute("disabled");
-    blockButton.innerHTML = "block input";
+  const blockButton = document.getElementById("block");
+  const mainInput = document.getElementById("input");
+  if (blockButton.innerHTML === "block input") {
+    mainInput.setAttribute("disabled", true);
+    blockButton.innerHTML = "unlock input";
+    return;
+  }
+  mainInput.removeAttribute("disabled");
+  blockButton.innerHTML = "block input";
 }
 
 function hideInput() {
-    const hideButton = document.getElementById("hide");
-    const mainInput = document.getElementById("input");
-    if (hideButton.innerHTML === "hide input") {
-        mainInput.classList.toggle("deleteButtonHidden");
-        hideButton.innerHTML = "show input";
-        return;
-    }
+  const hideButton = document.getElementById("hide");
+  const mainInput = document.getElementById("input");
+  if (hideButton.innerHTML === "hide input") {
     mainInput.classList.toggle("deleteButtonHidden");
-    hideButton.innerHTML = "hide input";
+    hideButton.innerHTML = "show input";
+    return;
+  }
+  mainInput.classList.toggle("deleteButtonHidden");
+  hideButton.innerHTML = "hide input";
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 function getRandomColorForInput() {
-    const mainInput = document.getElementById("input");
-    const random = getRandomInt(4);
-    const colors = ["black", "red", "green", "blue"];
-    console.log("color", colors[random])
-    mainInput.setAttribute("class", colors[random]);
+  const mainInput = document.getElementById("input");
+  const random = getRandomInt(4);
+  const colors = ["black", "red", "green", "blue"];
+  console.log("color", colors[random]);
+  mainInput.setAttribute("class", colors[random]);
 }
 
 function addTask() {
-    const mainInput = document.getElementById("input");
-    addElement(mainInput.value);
-    mainInput.value = "";
+  const mainInput = document.getElementById("input");
+  addElement(mainInput.value);
+  mainInput.value = "";
 }
 
 function addElement(value) {
-    const warnInput = document.getElementById("inputWarning");
-    const main = document.getElementById("main");
-    //select main element
-    const createTaskContainer = document.createElement("div");
-    createTaskContainer.setAttribute("class", "task");
-    const id = crypto.randomUUID();
-    createTaskContainer.setAttribute("id", "div" + id);
+  const warnInput = document.getElementById("inputWarning");
+  const main = document.getElementById("main");
+  //select main element
+  const createTaskContainer = document.createElement("div");
+  createTaskContainer.setAttribute("class", "task");
+  const id = crypto.randomUUID();
+  createTaskContainer.setAttribute("id", "div" + id);
 
-    if (!value.trim()) {
-        warnInput.setAttribute("class", "deletedTasksList");
-        return;
-    }
-    //set task name value to container
-    const taskName = document.createTextNode(value.trim());
-    createTaskContainer.appendChild(taskName);
-    warnInput.setAttribute("class", "deletedTasksListHidden");
+  if (!value.trim()) {
+    warnInput.setAttribute("class", "deletedTasksList");
+    return;
+  }
+  //set task name value to container
+  const taskName = document.createTextNode(value.trim());
+  createTaskContainer.appendChild(taskName);
+  warnInput.setAttribute("class", "deletedTasksListHidden");
 
-    main.appendChild(createTaskContainer);
+  main.appendChild(createTaskContainer);
 }
 
 function removeTask() {
-    const main = document.getElementById("main");
-    main.removeChild(main.lastChild)
+  const main = document.getElementById("main");
+  if (main.hasChildNodes()) {
+    main.removeChild(main.lastChild);
+  }
 }
 
 function removeBlock() {
-    const main = document.getElementById("removeElement");
-    // const element = document.getElementById("main").childNodes[main.value];
-    document.getElementById("main").removeChild(document.getElementById("main").children[main.value - 1]);
-}
+  const mainBlock = document.getElementById("main");
+  const mainInput = document.getElementById("removeElement");
 
+  if (mainBlock.childNodes.length > mainInput.value) {
+    document.getElementById("main").removeChild(document.getElementById("main").children[mainInput.value - 1]);
+  }
+}
